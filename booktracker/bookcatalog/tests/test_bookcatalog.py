312ad0364fe1b263adf2af49a1dfaf9c83d0dd4c
mack-book-review <79770053+mack-book-review@users.noreply.github.com>
2022-10-07 21:24:41 -0500

    Update test_bookcatalog.py

--- booktracker/bookcatalog/tests/test_bookcatalog.py
+++ booktracker/bookcatalog/tests/test_bookcatalog.py
@@ -1,12 +1,10 @@
 '''
     Tests for the BookCatalog API
 '''
-from django.test import TestCase
+
 from django.contrib.auth import get_user_model 
 from django.urls import reverse 
 
-from rest_framework.test import APIClient
-from rest_framework import status
 
 CREATE_BOOK_URL = reverse("book-create")
 
