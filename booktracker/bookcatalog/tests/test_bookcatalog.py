0b12f483c7ff816f97f7148bf96a2e77becbb3c3
mack-book-review <79770053+mack-book-review@users.noreply.github.com>
2022-10-07 21:03:54 -0500

    Update test_bookcatalog.py

--- booktracker/bookcatalog/tests/test_bookcatalog.py
+++ booktracker/bookcatalog/tests/test_bookcatalog.py
@@ -1,7 +1,6 @@
 '''
     Tests for the BookCatalog API
 '''
-
 from django.test import TestCase
 from django.contrib.auth import get_user_model 
 from django.urls import reverse 
@@ -10,24 +9,7 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 CREATE_BOOK_URL = reverse("book-create")
-CREATE_BOOK_REVIEW = reverse("review-create")
 
-
-def test_homepage_access(self):
+def test_homepage_access():
         url = reverse("home")
         assert url == "/"
-        
-# def create_user(**params):
-#     '''Create and return a new user'''
-#     return get_user_model().objects.create_user(params)
-
-
-# class BookCatalogTests(TestCase):
-    
-#     def setUp(self):
-#         self.client = APIClient()
-        
-#     def test_bookcatalog(self):
-#         '''
-#             Test
-#         '''
